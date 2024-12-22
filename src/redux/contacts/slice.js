3a7ecf292b292573a10dd9bf0e4fd46480a85a98
Olena <alchimeraa@gmail.com>
2024-12-22 20:22:17 +0200

    src

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -1,26 +1,3 @@
-// import { createSlice } from '@reduxjs/toolkit';
-
-// const contactsSlice = createSlice({
-//   name: 'contacts',
-//   initialState: {
-//     items: [],
-//   },
-//   reducers: {
-//     addContact: (state, action) => {
-//       state.items.push(action.payload);
-//     },
-//     deleteContact: (state, action) => {
-//       state.items = state.items.filter(contact => contact.id !== action.payload);
-//     },
-//   },
-// });
-
-
-// export const contactsReducer = contactsSlice.reducer;
-// export const { addContact, deleteContact } = contactsSlice.actions;
-
-// export default contactsSlice.reducer;
-
 import { createSlice } from '@reduxjs/toolkit';
 import { fetchContacts } from './operations';
 
